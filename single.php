5ccb3544b0fe5d831cbbf2dd8f2f122f401826d9
Laura Junco <lc.junco10@uniandes.edu.co>
2021-05-20 17:46:27 -0500

    Agregar content single

--- single.php
+++ single.php
@@ -15,20 +15,8 @@ get_header();
 		<?php
 		while ( have_posts() ) :
 			the_post();
-
-			get_template_part( 'template-parts/content', get_post_type() );
-
-			the_post_navigation(
-				array(
-					'prev_text' => '<span class="nav-subtitle">' . esc_html__( 'Previous:', 'aquivivegente' ) . '</span> <span class="nav-title">%title</span>',
-					'next_text' => '<span class="nav-subtitle">' . esc_html__( 'Next:', 'aquivivegente' ) . '</span> <span class="nav-title">%title</span>',
-				)
-			);
-
-			// If comments are open or we have at least one comment, load up the comment template.
-			if ( comments_open() || get_comments_number() ) :
-				comments_template();
-			endif;
+			
+			get_template_part( 'template-parts/content-single' );
 
 		endwhile; // End of the loop.
 		?>
@@ -36,5 +24,5 @@ get_header();
 	</main><!-- #main -->
 
 <?php
-get_sidebar();
+
 get_footer();
---
+++ template-parts/content-single.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package aquivivegente
+ */
+
+?>
+
+<div class="content-container <?php the_field( 'color' ); ?> <?php the_field( 'clase' ); ?>">
+
+<h2><?php the_title(); ?></h2>
+<p> 
+A lo largo de las últimas tres semanas en este espacio se cosieron cuatro telas como parte de un proceso colectivo de memoria y tejido. Para diseñarlas participamos de talleres de memoria escrita y de creación en tela liderados por la Unión de Costurero, en los que se dibujaban o escribían las experiencias que todos hemos tenido con diferentes formas o manifestaciones de la violencia, para luego diseñar unas telas en colectivo con simbología sobre eso o para hablar de las experiencias de unión, sororidad y afecto en las organizaciones a las que pertenecemos. 
+La tela central “Aquí vive gente” recoge imágenes de las cuatro organizaciones locales que conforman este proyecto: Unión de Costurero, Casa B (a través del Boletín Periférico y Círculo de Mujeres Aisha), Espacio Odeón y el Centro No Se Vende. Y es una representación de nuestro proceso de trabajo en colectivo durante los últimos 3 meses. 
+<br>
+<br>
+“El árbol del desplazamiento” narra la historia de un proceso de desplazamiento, cómo una familia tuvo que salir de su lugar de origen a causa de un hecho victimizante. Las hojas que se están cayendo, así como las personas, representan lo que se queda en el camino; mientras que las casas y la naturaleza se refieren al territorio y la vida que se tuvo que abandonar. 
+Las telas de Círculo de Mujeres Aisha y Espacio Odeón fueron procesos de encuentro entre los equipos de las organizaciones, maneras de conocernos de forma más profunda y de representarnos y crear una memoria de nuestros procesos juntos. En el primer caso la tela toma la sororidad, la unión entre mujeres y la cocina como lo central a la experiencia del Círculo de Mujeres Aisha; en el segundo se presenta el edificio Odeón, el equipo de personas, el arte, los procesos de trabajo y la fiesta como formas representativas de nuestra historia.
+</p>
+
+</div>
