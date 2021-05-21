9c359e42257248dc5f60704b77271371f00bcadf
Laura Junco <lc.junco10@uniandes.edu.co>
2021-05-21 11:41:40 -0500

    Agregar img grilla

---
+++ template-parts/content-single-telas.php
@@ -0,0 +1,19 @@
+<div class="col-lg-5 media-col">
+  
+  <?php if( get_field('video') ): ?>
+    <div class="video-container">
+        <img class="grilla" src="<?php bloginfo( 'template_url' )?>/assets/grilla_amarilla_hor.svg" alt="">
+
+        <iframe class="video" allowFullScreen src="<?php the_field('video'); ?>"></iframe>  
+    </div>
+  <?php endif; ?>
+  
+  <?php if( get_field('img1') ): ?>
+    <div class="img-container m-top">
+        <img class="grilla" src="<?php bloginfo( 'template_url' )?>/assets/grilla_amarilla_hor.svg" alt="">
+
+        <img class="img-content" src="<?php the_field('img1'); ?>" alt="">
+    </div>
+  <?php endif; ?>
+
+</div>
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -12,8 +12,13 @@
 <div class="content-container <?php the_field( 'color' ); ?> <?php the_field( 'clase' ); ?>">
 
   <div class="row">
-    <div class="col-lg-5 col-lg-offset-1">
+    <div class="col-lg-11 col-lg-offset-1">
       <h2><?php the_title(); ?></h2>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-lg-5 col-lg-offset-1">
       <?php the_content(); ?>
     </div>
     <?php get_template_part( 'template-parts/content-single', basename(get_permalink()) );?>
