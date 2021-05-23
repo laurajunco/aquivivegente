7a1d14b63faf16410dd327e980fab582ad1a9285
Laura Junco <lc.junco10@uniandes.edu.co>
2021-05-23 14:34:49 -0500

    Avances contenido

---
+++ template-parts/content-single-info.php
@@ -0,0 +1,10 @@
+<div class="col-lg-5 media-col">
+  
+  <?php if( get_field('img1') ): ?>
+    <div class="img-container">
+        <img class="grilla" src="<?php echo esc_url( get_template_directory_uri() )?>/assets/grilla_morada.svg" alt="">
+        <img class="img-content" src="<?php the_field('img1'); ?>" alt="">
+    </div>
+  <?php endif; ?>
+
+</div>
---
+++ template-parts/content-single-mapa.php
@@ -0,0 +1,27 @@
+  <div class="col-lg-5 media-col">
+
+    <?php if( get_field('img1') ): ?>
+      <div class="img-container">
+          <img class="grilla" src="<?php echo esc_url( get_template_directory_uri() )?>/assets/grilla_amarilla_hor.svg" alt="">
+          <img class="img-content" src="<?php the_field('img1'); ?>" alt="">
+      </div>
+    <?php endif; ?>
+
+    <?php if( get_field('enlace') ): ?>
+    <a class="content-link" href="<?php the_field('enlace'); ?>"><?php the_field('texto_enlace'); ?></a>
+    <?php endif; ?>
+  </div>
+</div> <!-- end Row -->
+
+<div class="row m-top-50">
+  <?php if( get_field('texto1') ): ?>
+    <div class="col-lg-5 col-lg-offset-1 ">
+      <p> <?php the_field('texto1'); ?></p>
+    </div>
+  <?php endif; ?>
+
+  <?php if( get_field('texto2') ): ?>
+    <div class="col-lg-5 media-col">
+      <p> <?php the_field('texto2'); ?></p>
+    </div>
+  <?php endif; ?>
