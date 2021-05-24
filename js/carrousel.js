3bdff6f47628e6f7cc794afd69c3a228664797f8
Laura Junco <lc.junco10@uniandes.edu.co>
2021-05-24 12:06:35 -0500

    Agregar Interacción boton next

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,7 +1,10 @@
 (function ($, root, undefined) {
   $('.galeria').slick({
     slidesToShow: 3.5,
-    initialSlide: 0.5
+    initialSlide: 0.5,
+    slidesToScroll: 1,
+    prevArrow: null,
+    nextArrow: $('#objetos-next')
   });
 })(jQuery, this);
 
--- template-parts/content-single-antecedentes.php
+++ template-parts/content-single-antecedentes.php
@@ -53,7 +53,10 @@
     <img class="img-galeria" src="<?php echo $galeria['img_11'] ?>" />
     <img class="img-galeria" src="<?php echo $galeria['img_12'] ?>" />
   </div>
-  
-  <span class="slick-next" id ="objetos-next">Objetos del museo </span> 
-  <img src="<?php echo esc_url( get_template_directory_uri() )?>/assets/next-arrow.svg">
 </div>
+
+<div class="row">
+  <div class="col-lg-10 col-lg-offset-1 slick-next" id="objetos-next"> 
+    <span ><strong>Objetos del museo</strong> </span> 
+    <img src="<?php echo esc_url( get_template_directory_uri() )?>/assets/next-arrow.svg">
+  </div>
