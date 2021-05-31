22ff754322189d738d1bb04db4d00a87f18af629
Laura Junco <lc.junco10@uniandes.edu.co>
2021-05-31 11:40:07 -0500

    Seguir mostrando objetos

--- template-parts/content-single-objetos.php
+++ template-parts/content-single-objetos.php
@@ -1,1 +1,13 @@
-<h1>Objetos!!!</h1>
+<div class="row">
+  <div class="col-lg-10 col-lg-offset-1 objects">
+
+  <?php query_posts('post_type=objeto&posts_per_page=14&orderby=date');?>
+  
+  <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <?php get_template_part( 'template-parts/content', get_post_type() ); ?>
+  <?php 
+    endwhile;
+    endif;
+  ?>
+  </div>
+</div>
