5ebe845819f66a1a128cb8784220760d7255d559
Laura Junco <lc.junco10@uniandes.edu.co>
2021-05-31 11:35:13 -0500

    Custom template for objetos

---
+++ template-parts/objeto.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Template Name: Objetos
+Template Post Type: post
+*/
+ 
+get_header();
+?>
+	<main id="primary" class="site-main single-main" data-barba="container" data-barba-namespace="single">
+
+	<div class="content-container <?php the_field( 'color' ); ?> <?php the_field( 'clase' ); ?>">
+
+  <div class="row">
+    <div class="col-lg-11 col-lg-offset-1">
+      <h2><?php the_title(); ?></h2>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-lg-5 col-lg-offset-1">
+      <?php the_content(); ?>
+    </div>
+  </div>
+
+  <?php get_template_part( 'template-parts/content-single', basename(get_permalink()) );?>
+  
+  <div class="sidebar "> 
+    <a href="<?php echo get_permalink( get_option( 'page_for_posts' ) ); ?>"><img src="<?php echo esc_url( get_template_directory_uri() )?>/assets/close.svg"></a>
+
+    <?php if( get_field('img1') ): ?>
+      <p><?php the_field( 'side-title' ); ?></p>
+    <?php endif; ?>
+  </div>
+
+  <div class="subir"> 
+  <img src="<?php echo esc_url( get_template_directory_uri() )?>/assets/arrow-up.svg"> <span >Subir</span> 
+    
+  </div>
+
+</div>
+
+	</main><!-- #main -->
+<?php
+
+get_footer();
+
+
