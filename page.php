d5b86e637acd79aae054e58bfa96d331c635e0ea
Laura Junco <lc.junco10@uniandes.edu.co>
2021-05-31 14:27:45 -0500

    Fix page template

--- page.php
+++ page.php
@@ -1,11 +1,23 @@
 <?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package aquivivegente
+ */
 
 get_header();
 ?>
 	<main id="primary" class="site-main single-main" data-barba="container" data-barba-namespace="single">
 	<div class="content-container">
   <div class="row">
-  <div class="col-lg-11 col-lg-offset-1">
+  <div class="col-lg-11 col-lg-offset-1 amarillo">
       
   <!-- carrusel acá -->
   </div>
