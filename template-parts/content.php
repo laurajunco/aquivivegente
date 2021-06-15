2b9c1d72014b3ee11bcf401e97e34647cabcceff
Laura Junco <lc.junco10@uniandes.edu.co>
2021-06-15 14:58:11 -0500

    Arreglos nuevas imagenes

--- template-parts/content.php
+++ template-parts/content.php
@@ -11,7 +11,7 @@
 
 <div class="card-container col col-lg-<?php the_field( 'columnas' ); ?> col-lg-offset-<?php the_field( 'offset' );?> col-md-6 col-md-offset-0 col-sm-6"> 
 	<div class="card <?php the_field( 'color' ); ?> <?php the_field( 'clase' ); ?>">
-		<?php the_post_thumbnail(); ?>
+	<img src="<?php echo get_the_post_thumbnail_url(); ?>" alt="">
 		<div class="card-content">
 			<h3><?php the_title(); ?></h3>
 			<p> <?php the_excerpt(); ?></p>
