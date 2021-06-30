c7f5b55de3fbecf127963bcdc1947ee9117422af
Laura Junco <lc.junco10@uniandes.edu.co>
2021-06-30 18:46:34 -0500

    Agregar nuevas imagenes acerca de

--- template-parts/content-single-info.php
+++ template-parts/content-single-info.php
@@ -1,10 +1,28 @@
-  <div class="col-lg-5 col-lg-offset-0 media-col sticky col-md-9 col-md-offset-1">
+  <div class="col-lg-5 col-lg-offset-0 media-col col-md-9 col-md-offset-1">
     
     <?php if( get_field('img1') ): ?>
       <div class="img-container">
           <img class="grilla" src="<?php echo esc_url( get_template_directory_uri() )?>/assets/grilla_morada.svg" alt="">
           <a href="#" class="open-modal"> 
             <img class="img-content" src="<?php the_field('img1'); ?>" alt="">
+          </a>
+      </div>
+    <?php endif; ?>
+
+    <?php if( get_field('img2') ): ?>
+      <div class="img-container m-top">
+          <img class="grilla" src="<?php echo esc_url( get_template_directory_uri() )?>/assets/grilla_morada.svg" alt="">
+          <a href="#" class="open-modal"> 
+            <img class="img-content" src="<?php the_field('img2'); ?>" alt="">
+          </a>
+      </div>
+    <?php endif; ?>
+
+    <?php if( get_field('img3') ): ?>
+      <div class="img-container m-top">
+          <img class="grilla" src="<?php echo esc_url( get_template_directory_uri() )?>/assets/grilla_morada.svg" alt="">
+          <a href="#" class="open-modal"> 
+            <img class="img-content" src="<?php the_field('img3'); ?>" alt="">
           </a>
       </div>
     <?php endif; ?>
