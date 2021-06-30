803cec429a99c618ebffb5c3c9b5dddc67832dd4
Laura Junco <lc.junco10@uniandes.edu.co>
2021-06-30 16:25:10 -0500

    Agregar nuevo texto a objetos

--- template-parts/content-single-objetos.php
+++ template-parts/content-single-objetos.php
@@ -1,7 +1,7 @@
 <div class="row">
   <div class="col-lg-10 col-lg-offset-1 objetos-container col-md-10 col-md-offset-1">
 
-  <?php query_posts('post_type=objeto&posts_per_page=14&orderby=date');?>
+  <?php query_posts('post_type=objeto&posts_per_page=15&orderby=date');?>
   
   <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
     <?php get_template_part( 'template-parts/content', get_post_type() ); ?>
