7cdda6cde43c1862799acaf99d38c57e094f5cff
Laura Junco <lc.junco10@uniandes.edu.co>
2021-06-30 16:08:20 -0500

    Agregar texto a objetos

--- template-parts/content-objeto.php
+++ template-parts/content-objeto.php
@@ -9,9 +9,14 @@
 
 ?>
 
-<div class="card-container col col-lg-<?php the_field( 'columnas' ); ?> col-lg-offset-<?php the_field( 'offset' );?> col-md-6 col-md-offset-0 col-sm-6" > 
+<?php if( get_field('clase') ): ?>	
+	<div class="card-container col col-lg-<?php the_field( 'columnas' ); ?> col-lg-offset-<?php the_field( 'offset' );?> col-md-12 col-md-offset-0 col-sm-12" > 	
+		<?php else : ?>
+	<div class="card-container col col-lg-<?php the_field( 'columnas' ); ?> col-lg-offset-<?php the_field( 'offset' );?> col-md-6 col-md-offset-0 col-sm-6" > 		
+<?php endif; ?>
+
 	<div class="open-modal-obj card <?php the_field( 'color' ); ?> <?php the_field( 'clase' ); ?> <?php the_field( 'orientacion' ); ?>">
-	
+
 		<img src=<?php the_field( 'img' ); ?> class="attachment-post-thumbnail size-post-thumbnail wp-post-image" alt="" loading="lazy">
 
 		<div class="card-content">
